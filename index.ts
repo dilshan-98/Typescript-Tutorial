d86b2bd5f7849dfa57fd1912e4386fc948c99087
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-06-20 03:50:48 +0530

    completed the compiler, part 1

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-let id: number = 5;
+// 1. How to compile Typescript to Javascript using Typescript Compiler 
 
-id = "5";
+/*let id: number = 5;
+
+id = "5";    ----> This will output an error because "id" is a number variable.
+
+To continuously watch the changes to the file in TS, use command "tsc.cmd --watch .\index.ts". This will update when--
+--changes are done to the file.*/
+
+
+
+// 2. Setup tsconfig file
+
+//command "tsc.cmd --init" will generate the "tsconfig.json" file.
